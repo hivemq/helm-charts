0b24628f57e6b791a28710b1fa0a7c9851e0b3d4
Santiago Hurtado <santiago.hurtado@hivemq.com>
2022-03-01 12:37:47 +0000

    test: deploy using helm example

--- src/test/java/com/hivemq/helmcharts/deploymentIT.java
+++ src/test/java/com/hivemq/helmcharts/deploymentIT.java
@@ -11,6 +11,7 @@ import org.testcontainers.utility.DockerImageName;
 import java.io.File;
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.testcontainers.containers.BindMode.READ_ONLY;
 
@@ -18,20 +19,43 @@ public class deploymentIT {
 
     private final @NotNull Logger log = LoggerFactory.getLogger(deploymentIT.class);
 
-
     @Test
     public void deployCluster() throws IOException, ApiException, InterruptedException {
-        var k3sContainer = new K3sContainer(DockerImageName.parse("kube:latest").asCompatibleSubstituteFor("rancher/k3s"));
-        k3sContainer.addFileSystemBind(new File(".").getCanonicalPath(), "/test", READ_ONLY);
-        k3sContainer.start();
-        var config = k3sContainer.getKubeConfigYaml();
-        var out = k3sContainer.execInContainer("/bin/kubectl", "get", "nodes");
-        System.out.println(out.getStdout());
-        System.err.println(out.getStderr());
-        k3sContainer.getLogs();
-        boolean isCreated = k3sContainer.isCreated();
-        assertTrue(isCreated);
-        k3sContainer.close();
+        try (var k3sContainer = new K3sContainer(DockerImageName.parse("kube:latest").asCompatibleSubstituteFor("rancher/k3s"))) {
+            k3sContainer.addFileSystemBind(new File(".").getCanonicalPath(), "/test", READ_ONLY);
+            k3sContainer.addExposedPort(1883);
+            k3sContainer.start();
+            var port = k3sContainer.getMappedPort(1883);
+            System.out.println(port);
+            boolean isCreated = k3sContainer.isCreated();
+            assertTrue(isCreated);
+            var config = k3sContainer.getKubeConfigYaml();
+            var outNodes = k3sContainer.execInContainer("/bin/kubectl", "get", "nodes");
+            System.out.println(outNodes.getStdout());
+            System.err.println(outNodes.getStderr());
+            //helm --kubeconfig /etc/rancher/k3s/k3s.yaml install hivemq .
+            var outDeploy = k3sContainer.execInContainer("/bin/helm",
+                    "--kubeconfig",
+                    "/etc/rancher/k3s/k3s.yaml",
+                    "install",
+                    "hivemq",
+                    "/test/charts/hivemq-operator",
+                    "-f",
+                    "/test/src/test/resources/testValues.yaml");
+            System.out.println(outDeploy.getStdout());
+            System.err.println(outDeploy.getStderr());
+            assertFalse(outDeploy.getStderr().isEmpty());
+            assertTrue(outDeploy.getStdout().contains("STATUS: deployed"));
+            var outExpose = k3sContainer.execInContainer("/bin/kubectl",
+                    "expose",
+                    "deployment",
+                    "hivemq",
+                    "--type=LoadBalancer",
+                    "--name=hivemq-external");
+            System.out.println(outExpose.getStdout());
+            System.err.println(outExpose.getStderr());
+            assertFalse(outExpose.getStderr().isEmpty());
+        }
     }
 
 }
