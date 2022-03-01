04a42acd8d2322944def2941b616c8d48fd240ca
Santiago Hurtado <santiago.hurtado@hivemq.com>
2022-03-01 15:08:09 +0100

    test: use fabric8

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,9 +17,9 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testImplementation("org.testcontainers:testcontainers:1.16.3")
     testImplementation("org.testcontainers:k3s:1.16.3")
-    testImplementation("io.kubernetes:client-java:14.0.0")
     testImplementation("org.slf4j:slf4j-api:1.7.36")
     testImplementation("org.slf4j:slf4j-simple:1.7.36")
+    testImplementation("io.fabric8:kubernetes-client:5.12.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
     testImplementation("org.testcontainers:junit-jupiter:1.16.3")
 }
--- src/test/java/com/hivemq/helmcharts/deploymentIT.java
+++ src/test/java/com/hivemq/helmcharts/deploymentIT.java
@@ -1,23 +1,20 @@
 package com.hivemq.helmcharts;
 
 import com.hivemq.helmcharts.util.HelmK3sContainer;
-import io.kubernetes.client.openapi.ApiClient;
-import io.kubernetes.client.openapi.ApiException;
-import io.kubernetes.client.openapi.apis.CoreV1Api;
-import io.kubernetes.client.util.Config;
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testcontainers.k3s.K3sContainer;
-import org.testcontainers.utility.DockerImageName;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.StringReader;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.testcontainers.containers.BindMode.READ_ONLY;
 
 public class deploymentIT {
@@ -65,7 +62,7 @@ public class deploymentIT {
         }
     }*/
     @Test
-    public void testCustomContainer() throws IOException, ApiException, InterruptedException {
+    public void testCustomContainer() throws IOException, InterruptedException {
         int mqttPort = 1883;
         container = HelmK3sContainer.builder()
                 .k3sVersion("v1.21.10-k3s1")
@@ -76,11 +73,10 @@ public class deploymentIT {
         exposeService(configYml,container.getMappedPort(mqttPort));
     }
 
-    private void exposeService(final @NotNull String configYml,int port) throws IOException, ApiException {
-        ApiClient client = Config.fromConfig(new StringReader(configYml));
-        CoreV1Api api = new CoreV1Api(client);
-        var nodes = api.listNode(null,null,null,null,null,null,null,null,null,null);
-        System.out.println(nodes.getItems().size());
+    private void exposeService(final @NotNull String configYml,int port) throws IOException {
+        Config config = Config.fromKubeconfig(configYml);
+        KubernetesClient client = new DefaultKubernetesClient(config);
+        System.out.println(client.services().list().getItems().size());
     }
 
     public void deployChart() throws IOException, InterruptedException {
