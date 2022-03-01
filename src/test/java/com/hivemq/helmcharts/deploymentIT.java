5243a6a015fe7ca4bcad3689929b08679bea2471
Santiago Hurtado <santiago.hurtado@hivemq.com>
2022-03-01 14:59:00 +0100

    feat: extend container

--- src/test/java/com/hivemq/helmcharts/deploymentIT.java
+++ src/test/java/com/hivemq/helmcharts/deploymentIT.java
@@ -1,7 +1,12 @@
 package com.hivemq.helmcharts;
 
+import com.hivemq.helmcharts.util.HelmK3sContainer;
+import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.openapi.ApiException;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.util.Config;
 import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,17 +15,18 @@ import org.testcontainers.utility.DockerImageName;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.StringReader;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.testcontainers.containers.BindMode.READ_ONLY;
 
 public class deploymentIT {
 
     private final @NotNull Logger log = LoggerFactory.getLogger(deploymentIT.class);
+    public HelmK3sContainer container;
 
-    @Test
-    public void deployCluster() throws IOException, ApiException, InterruptedException {
+    /*@Test
+    public void deployCluster() throws IOException, InterruptedException {
         try (var k3sContainer = new K3sContainer(DockerImageName.parse("kube:latest").asCompatibleSubstituteFor("rancher/k3s"))) {
             k3sContainer.addFileSystemBind(new File(".").getCanonicalPath(), "/test", READ_ONLY);
             k3sContainer.addExposedPort(1883);
@@ -46,6 +52,7 @@ public class deploymentIT {
             System.err.println(outDeploy.getStderr());
             assertFalse(outDeploy.getStderr().isEmpty());
             assertTrue(outDeploy.getStdout().contains("STATUS: deployed"));
+            //We can not run this until the hivemq pod is ready
             var outExpose = k3sContainer.execInContainer("/bin/kubectl",
                     "expose",
                     "deployment",
@@ -55,6 +62,55 @@ public class deploymentIT {
             System.out.println(outExpose.getStdout());
             System.err.println(outExpose.getStderr());
             assertFalse(outExpose.getStderr().isEmpty());
+        }
+    }*/
+    @Test
+    public void testCustomContainer() throws IOException, ApiException, InterruptedException {
+        int mqttPort = 1883;
+        container = HelmK3sContainer.builder()
+                .k3sVersion("v1.21.10-k3s1")
+                .tempDir(new File("./src/test/resources"))
+                .build();
+        final String configYml = prepareContainer(mqttPort);
+        deployChart();
+        exposeService(configYml,container.getMappedPort(mqttPort));
+    }
+
+    private void exposeService(final @NotNull String configYml,int port) throws IOException, ApiException {
+        ApiClient client = Config.fromConfig(new StringReader(configYml));
+        CoreV1Api api = new CoreV1Api(client);
+        var nodes = api.listNode(null,null,null,null,null,null,null,null,null,null);
+        System.out.println(nodes.getItems().size());
+    }
+
+    public void deployChart() throws IOException, InterruptedException {
+        //helm --kubeconfig /etc/rancher/k3s/k3s.yaml install hivemq /test/charts/hivemq-operator -f /test/src/test/resources/testValues.yaml
+        var outDeploy = container.execInContainer("/bin/helm",
+                "--kubeconfig",
+                "/etc/rancher/k3s/k3s.yaml",
+                "install",
+                "hivemq",
+                "/test/charts/hivemq-operator",
+                "-f",
+                "/test/src/test/resources/testValues.yaml");
+        System.out.println(outDeploy.getStdout());
+        System.err.println(outDeploy.getStderr());
+        assertFalse(outDeploy.getStderr().isEmpty());
+        assertTrue(outDeploy.getStdout().contains("STATUS: deployed"));
+    }
+
+    private String prepareContainer(int port) throws IOException {
+        container.addFileSystemBind(new File(".").getCanonicalPath(), "/test", READ_ONLY);
+        container.addExposedPort(port);
+        container.start();
+        assertTrue(container.isCreated());
+        return container.getKubeConfigYaml();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (container != null) {
+            container.stop();
         }
     }
 
