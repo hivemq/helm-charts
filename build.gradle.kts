4dde272ac0425169a53ea8113860f70885847eea
Santiago Hurtado <santiago.hurtado@hivemq.com>
2022-03-11 15:29:47 +0100

    Use fabric8 to use the operator custom resources

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,11 @@ group = "com.hivemq.helmcharts"
 
 repositories {
     mavenCentral()
+    maven {
+        name = "operator"
+        url = uri("https://maven.pkg.github.com/hivemq/hivemq-operator")
+        credentials(PasswordCredentials::class)
+    }
 }
 
 dependencies {
@@ -17,8 +22,12 @@ dependencies {
     testImplementation("org.testcontainers:junit-jupiter:${property("testcontainers.version")}")
     testImplementation("org.slf4j:slf4j-api:${property("slf4j.version")}")
     testImplementation("org.slf4j:slf4j-simple:${property("slf4j.version")}")
-    testImplementation("io.kubernetes:client-java:${property("kubernetes.client.version")}")
     testImplementation("com.hivemq:hivemq-mqtt-client:${property("hivemq.client.version")}")
+    testImplementation("com.hivemq.operator:operator:${property("hivemq.operator.version")}")
+    testImplementation("io.fabric8:kubernetes-client:${property("fabric8.version")}")
+    testImplementation("org.bouncycastle:bcprov-jdk15on:${property("bouncycastle.version")}")
+    testImplementation("org.bouncycastle:bcpkix-jdk15on:${property("bouncycastle.version")}")
+
 }
 
 tasks.getByName<Test>("test") {
--- src/integrationTest/java/com/hivemq/helmcharts/HelmChartDeploymentIT.java
+++ src/integrationTest/java/com/hivemq/helmcharts/HelmChartDeploymentIT.java
@@ -1,17 +1,26 @@
 package com.hivemq.helmcharts;
 
+import com.hivemq.client.mqtt.MqttGlobalPublishFilter;
+import com.hivemq.client.mqtt.datatypes.MqttQos;
+import com.hivemq.client.mqtt.mqtt5.Mqtt5BlockingClient;
+import com.hivemq.client.mqtt.mqtt5.Mqtt5Client;
+import com.hivemq.client.mqtt.mqtt5.message.publish.Mqtt5Publish;
+import com.hivemq.crd.hivemq.HiveMQInfo;
 import com.hivemq.helmcharts.util.OperatorHelmChartContainer;
-import com.hivemq.helmcharts.util.OperatorHelmChartUtils;
+import com.hivemq.openapi.HivemqClusterStatus;
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.Watcher;
+import io.fabric8.kubernetes.client.WatcherException;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.testcontainers.containers.Container.ExecResult;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.io.File;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,42 +31,86 @@ import static org.junit.jupiter.api.Assertions.*;
 @Testcontainers
 public class HelmChartDeploymentIT {
 
-    @Timeout(value = 15, unit = TimeUnit.MINUTES)
+    @Timeout(value = 20, unit = TimeUnit.MINUTES)
     @ParameterizedTest
     @ValueSource(strings = {"v1.18.20-k3s1", "v1.19.16-k3s1", "v1.20.15-k3s1", "v1.21.10-k3s1", "v1.22.7-k3s1", "v1.23.4-k3s1"})
     public void withHelmVersionDeployment_mqttMessagePublishedReceived(final @NotNull String version) throws Exception {
         try (var container = OperatorHelmChartContainer.builder()
                 .k3sVersion(version)
                 .dockerfile(new File("./src/integrationTest/resources/Dockerfile"))
-                .helmChartMountPath(new File("."))
-                .containerPath("/test")
-                .build()) {
+                .helmChartMountPath(new File(".")).containerPath("/test").build()) {
 
             container.start();
 
-            final var utils = new OperatorHelmChartUtils(container);
+            var valuesPath = new File("/test/src/integrationTest/resources/testValues.yaml");
+            var operatorPath = new File("/test/charts/hivemq-operator");
 
-            final ExecResult execDeploy = utils.deployChart(
-                    new File("/test/src/integrationTest/resources/testValues.yaml"),
-                    new File("/test/charts/hivemq-operator"));
+
+            final var outUpdate = container
+                    .execInContainer("/bin/helm", "dependency", "update", operatorPath.getAbsolutePath() + "/");
+
+            assertTrue(outUpdate.getStderr().isEmpty());
+
+            var execDeploy = container
+                    .execInContainer("/bin/helm", "--kubeconfig", "/etc/rancher/k3s/k3s.yaml", "install",
+                            "hivemq", operatorPath.getAbsolutePath(), "-f", valuesPath.getAbsolutePath());
+
             if (!execDeploy.getStderr().isEmpty()) {
                 System.err.println(execDeploy.getStderr());
+                fail("Can not deploy using helm:" + execDeploy.getStderr());
             }
+
             assertTrue(execDeploy.getStdout().contains("STATUS: deployed"));
 
-            final String podLog = utils.exposeService();
-            assertTrue(podLog.contains("Started HiveMQ in"));
+            waitForClusterToBeReady(container);
 
-            final var receivedMessage = utils.testMqttClient("Sending Message");
-            assertNotNull(receivedMessage);
+            Mqtt5BlockingClient client = Mqtt5Client.builder()
+                    .automaticReconnectWithDefaultConfig()
+                    .serverPort(container.getMappedPort(1883))
+                    .buildBlocking();
+            client.connect();
+
+            var publishes = client.publishes(MqttGlobalPublishFilter.ALL);
+            client.subscribeWith().topicFilter("test").send();
+            client.publishWith()
+                    .topic("test")
+                    .payload("Sending Message".getBytes(StandardCharsets.UTF_8))
+                    .qos(MqttQos.AT_LEAST_ONCE).send();
+            Mqtt5Publish receivedMessage = publishes.receive();
             assertTrue(receivedMessage.getPayload().isPresent());
-            assertEquals("Sending Message", StandardCharsets.UTF_8.decode(receivedMessage.getPayload()
-                    .get().asReadOnlyBuffer()).toString());
+            assertEquals("Sending Message", StandardCharsets.UTF_8.decode(receivedMessage
+                    .getPayload().get().asReadOnlyBuffer()).toString());
 
             container.stop();
         }
 
     }
 
+    private void waitForClusterToBeReady(final @NotNull OperatorHelmChartContainer container) throws Exception {
+        String kubeConfigYaml = container.getKubeConfigYaml();
+        Config config = Config.fromKubeconfig(kubeConfigYaml);
+        DefaultKubernetesClient client = new DefaultKubernetesClient(config);
 
+        var completableFutureResource = new CompletableFuture<Boolean>();
+        client.customResources(HiveMQInfo.class).watch(new Watcher<>() {
+            @Override
+            public void eventReceived(@NotNull Action action, @NotNull HiveMQInfo resource) {
+                try {
+                    if (resource.getStatus() != null
+                            && resource.getStatus().getState() != null
+                            && resource.getStatus().getState() == HivemqClusterStatus.State.RUNNING) {
+                        completableFutureResource.complete(true);
+                    }
+                } catch (Exception e) {
+                    completableFutureResource.complete(false);
+                }
+            }
+
+            @Override
+            public void onClose(@NotNull WatcherException cause) {
+                System.out.println("onClose");
+            }
+        });
+        assertTrue(completableFutureResource.get());
+    }
 }
