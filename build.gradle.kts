af5674e79621fad093cd74019101f2caafa91e7a
Santiago Hurtado <santiago.hurtado@hivemq.com>
2022-03-17 17:05:31 +0100

    Test deploy using different versions of Kubernetes

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    java
+}
+
+group = "com.hivemq.helmcharts"
+
+repositories {
+    mavenCentral()
+    maven {
+        name = "operator"
+        url = uri("https://maven.pkg.github.com/hivemq/hivemq-operator")
+        credentials(PasswordCredentials::class)
+    }
+    maven{
+        url = uri("https://jitpack.io")
+    }
+}
+
+dependencies {
+    implementation("org.codehaus.groovy:groovy-all:${property("groovy.version")}")
+    implementation("org.junit.jupiter:junit-jupiter:${property("junit.version")}")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:${property("junit.version")}")
+    testImplementation("org.testcontainers:testcontainers:${property("testcontainers.version")}")
+    testImplementation("org.testcontainers:k3s:${property("testcontainers.version")}")
+    testImplementation("org.testcontainers:junit-jupiter:${property("testcontainers.version")}")
+    testImplementation("org.slf4j:slf4j-api:${property("slf4j.version")}")
+    testImplementation("org.slf4j:slf4j-simple:${property("slf4j.version")}")
+    testImplementation("com.hivemq:hivemq-mqtt-client:${property("hivemq.client.version")}")
+    testImplementation("com.hivemq.operator:operator:${property("hivemq.operator.version")}")
+    testImplementation("io.fabric8:kubernetes-client:${property("fabric8.version")}")
+    testImplementation("org.bouncycastle:bcprov-jdk15on:${property("bouncycastle.version")}")
+    testImplementation("org.bouncycastle:bcpkix-jdk15on:${property("bouncycastle.version")}")
+    testImplementation("org.awaitility:awaitility:${property("awaitility.version")}")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+
+sourceSets.create("integrationTest") {
+    compileClasspath += sourceSets.main.get().output + sourceSets.test.get().output
+    runtimeClasspath += sourceSets.main.get().output + sourceSets.test.get().output
+}
+
+val integrationTestImplementation: Configuration by configurations.getting {
+    extendsFrom(configurations.testImplementation.get())
+}
+val integrationTestRuntimeOnly: Configuration by configurations.getting {
+    extendsFrom(configurations.testRuntimeOnly.get())
+}
---
+++ src/integrationTest/java/com/hivemq/helmcharts/util/OperatorHelmChartContainer.java
@@ -0,0 +1,85 @@
+package com.hivemq.helmcharts.util;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.images.builder.ImageFromDockerfile;
+import org.testcontainers.k3s.K3sContainer;
+import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.utility.MountableFile;
+
+import java.io.File;
+
+/**
+ * Container that includes the helm binary to be able to install the HiveMQ helm charts
+ */
+public class OperatorHelmChartContainer extends K3sContainer {
+    public final static int mqttPort = 1883;
+
+    private OperatorHelmChartContainer(final @NotNull String k3sVersion,
+                                       final @NotNull File dockerfile,
+                                       final @NotNull File helmChartMountPath,
+                                       final @NotNull String containerPath) {
+        super(getAdHocImageName(k3sVersion, dockerfile));
+        final MountableFile helmChartMountable = MountableFile.forHostPath(helmChartMountPath.getPath());
+        super.withCopyFileToContainer(helmChartMountable, containerPath);
+        super.addExposedPort(mqttPort);
+    }
+
+    public static @NotNull DockerImageName getAdHocImageName(final @NotNull String k3sVersion,
+                                                             final @NotNull File dockerfile) {
+        final String s = new ImageFromDockerfile().withDockerfile(
+                        dockerfile.toPath())
+                .withBuildArg("K3S_VERSION", k3sVersion)
+                .get();
+        return DockerImageName.parse(s).asCompatibleSubstituteFor("rancher/k3s");
+    }
+
+    public static @NotNull Builder builder() {
+        return new Builder();
+    }
+
+    public int getMappedPort() {
+        return super.getMappedPort(mqttPort);
+    }
+
+    public void waitForMqttService() {
+        super.withExposedPorts(mqttPort).waitingFor(Wait.forListeningPort());
+    }
+
+    public static class Builder {
+
+        private @Nullable File dockerfile;
+        private @Nullable File helmChartMountPath;
+        private @Nullable String k3sVersion;
+        private @Nullable String containerPath;
+
+        public @NotNull Builder dockerfile(final @NotNull File dockerfile) {
+            this.dockerfile = dockerfile;
+            return this;
+        }
+
+        public @NotNull Builder helmChartMountPath(final @NotNull File mountPath) {
+            this.helmChartMountPath = mountPath;
+            return this;
+        }
+
+        public @NotNull Builder k3sVersion(final @NotNull String k3sVersion) {
+            this.k3sVersion = k3sVersion;
+            return this;
+        }
+
+        public @NotNull Builder containerPath(final @NotNull String containerPath) {
+            this.containerPath = containerPath;
+            return this;
+        }
+
+        public @NotNull OperatorHelmChartContainer build() {
+            assert dockerfile != null;
+            assert k3sVersion != null;
+            assert helmChartMountPath != null;
+            assert containerPath != null;
+            return new OperatorHelmChartContainer(k3sVersion, dockerfile, helmChartMountPath, containerPath);
+        }
+    }
+}
