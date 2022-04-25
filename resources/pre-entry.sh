cbf989e9269bfc6d2c5281e84feb376589a9f766
Santiago Hurtado <santiago.hurtado@hivemq.com>
2022-04-25 13:34:50 +0000

    simplify image build dependencies

--- resources/pre-entry.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ "${HIVEMQ_CLUSTER_TRANSPORT_TYPE}" == "UDP" ]]; then
-    # shellcheck disable=SC2016
-    sed -i -e 's|<\!--TRANSPORT_TYPE-->|<udp><bind-address>${HIVEMQ_BIND_ADDRESS}</bind-address><bind-port>${HIVEMQ_CLUSTER_PORT}</bind-port><!-- disable multicast to avoid accidental cluster forming --><multicast-enabled>false</multicast-enabled></udp>|' /opt/hivemq/conf/config.xml
-elif [[ "${HIVEMQ_CLUSTER_TRANSPORT_TYPE}" == "TCP" ]]; then
-    # shellcheck disable=SC2016
-    sed -i -e 's|<\!--TRANSPORT_TYPE-->|<tcp><bind-address>${HIVEMQ_BIND_ADDRESS}</bind-address><bind-port>${HIVEMQ_CLUSTER_PORT}</bind-port></tcp>|' /opt/hivemq/conf/config.xml
-fi
--- resources/scripts/heap.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Create a heap dump to /opt/hivemq/heap-dump.hprof
-
-curl -L https://github.com/apangin/jattach/releases/download/v1.5/jattach > jattach
-chmod +x jattach
-./jattach 1 dumpheap heap-dump.hprof
-
