010651c9b1beb4d02f188853e19cb7046f9c4660
Santiago Hurtado <santiago.hurtado@hivemq.com>
2022-04-27 06:39:44 +0200

    setup gradle to test container

--- src/integrationTest/java/com/hivemq/helmcharts/util/TestUtils.java
+++ src/integrationTest/java/com/hivemq/helmcharts/util/TestUtils.java
@@ -12,7 +12,10 @@ import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.Watcher;
 import io.fabric8.kubernetes.client.WatcherException;
 import org.jetbrains.annotations.NotNull;
+import org.testcontainers.junit.jupiter.Container;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.CountDownLatch;
 
@@ -28,26 +31,30 @@ public class TestUtils {
      * @param kubeConfigYaml k3s yaml configuration for the container that is running and waiting for the Kubernetes artifacts to be ready
      */
     public static CountDownLatch getWaitForClusterToBeReadyLatch(final @NotNull String kubeConfigYaml) {
-        Config config = Config.fromKubeconfig(kubeConfigYaml);
-        DefaultKubernetesClient client = new DefaultKubernetesClient(config);
         final CountDownLatch closeLatch = new CountDownLatch(1);
-        client.customResources(HiveMQInfo.class).watch(new Watcher<>() {
-            @Override
-            public void eventReceived(@NotNull Action action, @NotNull HiveMQInfo resource) {
-                if (resource.getStatus() != null
-                        && resource.getStatus().getState() != null
-                        && resource.getStatus().getState() == HivemqClusterStatus.State.RUNNING) {
-                    closeLatch.countDown();
-                }
-            }
+        Config config = Config.fromKubeconfig(kubeConfigYaml);
 
-            @Override
-            public void onClose(@NotNull WatcherException cause) {
-                System.out.println("onClose");
-            }
-        });
+        try (DefaultKubernetesClient client = new DefaultKubernetesClient(config)) {
+
+            client.customResources(HiveMQInfo.class).watch(new Watcher<>() {
+                @Override
+                public void eventReceived(@NotNull Action action, @NotNull HiveMQInfo resource) {
+                    if (resource.getStatus() != null
+                            && resource.getStatus().getState() != null
+                            && resource.getStatus().getState() == HivemqClusterStatus.State.RUNNING) {
+                        closeLatch.countDown();
+                    }
+                }
+
+                @Override
+                public void onClose(@NotNull WatcherException cause) {
+                    System.out.println("onClose");
+                }
+            });
+        }
         return closeLatch;
     }
+
     public static void sendTestMessage(int mappedPort) throws InterruptedException {
         Mqtt5BlockingClient client = Mqtt5Client.builder()
                 .automaticReconnectWithDefaultConfig()
@@ -65,6 +72,38 @@ public class TestUtils {
         assertTrue(receivedMessage.getPayload().isPresent());
         assertEquals("Sending Message", StandardCharsets.UTF_8.decode(receivedMessage
                 .getPayload().get().asReadOnlyBuffer()).toString());
+    }
+
+    /**
+     * Deploys an operator from local resources
+     *
+     * @param container container to execute the command
+     * @param resourcesPath where the resources are mounted on the image
+     * @param customValuesPath where are the custom values located for the helm chart
+     * @return String from the execute command
+     */
+    @NotNull
+    public static String deployLocalOperator(OperatorHelmChartContainer container,
+                                             @NotNull String resourcesPath,
+                                             @NotNull String customValuesPath) throws Exception {
+
+        var operatorPath = new File(resourcesPath+"/charts/hivemq-operator").getAbsolutePath();
+
+
+        final var outUpdate = container
+                .execInContainer("/bin/helm", "dependency", "update", operatorPath + "/");
+
+        assertTrue(outUpdate.getStderr().isEmpty());
+
+        var execDeploy = container
+                .execInContainer("/bin/helm", "--kubeconfig", "/etc/rancher/k3s/k3s.yaml", "install",
+                        "hivemq", operatorPath, "-f", customValuesPath);
+
+        if (!execDeploy.getStderr().isEmpty()) {
+            // Shows also warnings
+            System.err.println(execDeploy.getStderr());
+        }
+        return execDeploy.getStdout();
     }
 
 }
