80a9f69fd411094c47ba03e5d48b76c475907453
Santiago Hurtado <santiago.hurtado@hivemq.com>
2022-05-03 05:16:24 -0500

    move to wait condition for the container

--- src/integrationTest/java/com/hivemq/helmcharts/HelmChartDeploymentIT.java
+++ src/integrationTest/java/com/hivemq/helmcharts/HelmChartDeploymentIT.java
@@ -1,57 +1,28 @@
 package com.hivemq.helmcharts;
 
-import com.hivemq.helmcharts.util.TestUtils;
 import com.hivemq.helmcharts.util.OperatorHelmChartContainer;
 import org.jetbrains.annotations.NotNull;
-import org.junit.jupiter.api.Timeout;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.MountableFile;
 
 import java.io.File;
-import java.util.concurrent.TimeUnit;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-/**
- * Test that the chart is deployed successfully on specific kubernetes cluster versions
- */
 @Testcontainers
 public class HelmChartDeploymentIT {
 
-    private static final @NotNull String resourcesPath = "/test";
-    private static final @NotNull String customValuesPath = resourcesPath+
-            "/src/integrationTest/resources/testValues.yaml";
+    @Container
+    private static final @NotNull OperatorHelmChartContainer
+            container = new OperatorHelmChartContainer("v1.23.4-k3s1",
+            "k3s.dockerfile",
+            "testValues.yaml");
+    @Test
+    public void withCustomImage_mqttMessagePublishedReceived() {
+        System.out.println("is running:" + container.isRunning());
 
+        System.out.println(Runtime.getRuntime().maxMemory());
 
-    @Timeout(value = 20, unit = TimeUnit.MINUTES)
-    @ParameterizedTest
-    @ValueSource(strings = {"v1.18.20-k3s1", "v1.19.16-k3s1", "v1.20.15-k3s1", "v1.21.10-k3s1", "v1.22.7-k3s1", "v1.23.4-k3s1"})
-    public void withHelmVersionDeployment_mqttMessagePublishedReceived(final @NotNull String version) throws Exception {
-        var customValues = new File(customValuesPath).getAbsolutePath();
-        try (var container = OperatorHelmChartContainer.builder()
-                .k3sVersion(version)
-                .withTmpFs(true)
-                .dockerfile("k3s.dockerfile")
-                .helmChartMountPath(new File(".")).containerPath(resourcesPath).build()) {
-
-            container.start();
-
-            var deploy = TestUtils.deployLocalOperator(container,resourcesPath,customValues);
-
-            assertTrue(deploy.contains("STATUS: deployed"));
-
-            assertTrue(TestUtils.getWaitForClusterToBeReadyLatch(container.getKubeConfigYaml()).await(3, TimeUnit.MINUTES));
-
-            TestUtils.sendTestMessage(container.getMappedPort(1883));
-
-            container.stop();
-        }
-
+        container.stop();
     }
-
-
-
 }
--- src/integrationTest/java/com/hivemq/helmcharts/UserPermissionsIT.java
+++ src/integrationTest/java/com/hivemq/helmcharts/UserPermissionsIT.java
@@ -19,40 +19,10 @@ public class UserPermissionsIT {
     private static final @NotNull String resourcesPath = "/test";
     private static final @NotNull String customValuesPath = resourcesPath+"/src/integrationTest/resources/permissionsDeployment.yaml";
 
-    @Container
-    private static final @NotNull OperatorHelmChartContainer container = OperatorHelmChartContainer.builder()
-            .k3sVersion("v1.20.15-k3s1")
-            .dockerfile("k3s.dockerfile")
-            .helmChartMountPath(new File(".")).containerPath(resourcesPath).build();
 
     @Test
     public void withCustomImage_mqttMessagePublishedReceived() throws Exception {
         // We need at least 6GB of RAM for this test
-        System.out.println(Runtime.getRuntime().maxMemory());
 
-        var customValues = new File(customValuesPath).getAbsolutePath();
-
-        container.start();
-
-        var containerPath = "/build/container/context/hivemq4-k8s-test.tar";
-
-        assertTrue(new File("."+containerPath).exists());
-
-        var outLoadImage = container.execInContainer("/bin/ctr",
-                "images",
-                "import",
-                resourcesPath+containerPath);
-
-        assertFalse(outLoadImage.getStdout().isEmpty());
-
-        var deploy = TestUtils.deployLocalOperator(container,resourcesPath,customValues);
-
-        assertTrue(deploy.contains("STATUS: deployed"));
-
-        assertTrue(TestUtils.getWaitForClusterToBeReadyLatch(container.getKubeConfigYaml()).await(3, TimeUnit.MINUTES));
-
-        TestUtils.sendTestMessage(container.getMappedPort(1883));
-
-        container.stop();
     }
 }
