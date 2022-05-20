984f05e98c9225d208d6bb5c386c77d34e4921c6
Santiago Hurtado <santiago.hurtado@hivemq.com>
2022-05-20 11:41:26 -0500

    starting container to ready

--- src/integrationTest/java/com/hivemq/helmcharts/HelmChartDeploymentIT.java
+++ src/integrationTest/java/com/hivemq/helmcharts/HelmChartDeploymentIT.java
@@ -14,9 +14,8 @@ public class HelmChartDeploymentIT {
     private static final @NotNull OperatorHelmChartContainer
             container = new OperatorHelmChartContainer("v1.23.4-k3s1",
             "k3s.dockerfile",
-            "customTestValues.yaml")
-            .withCopyFileToContainer(MountableFile.forClasspathResource("cluster.yml"),"/files/cluster.yml")
-            .withCopyFileToContainer(MountableFile.forClasspathResource("test_helm_chart.sh"),"/bin/test_helm_chart.sh");
+            "values/customTestValues.yaml")
+            .withCopyFileToContainer(MountableFile.forClasspathResource("manifest/cluster.yml"),"/files/cluster.yml");
 
 
     @Test
