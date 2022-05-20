555365a274ee631f79bf4370ed89056ac0c80d2e
Santiago Hurtado <santiago.hurtado@hivemq.com>
2022-05-20 10:12:04 -0500

    Testing to run on the m1

--- src/integrationTest/java/com/hivemq/helmcharts/CompatibilityHelmChartDeploymentIT.java
+++ src/integrationTest/java/com/hivemq/helmcharts/CompatibilityHelmChartDeploymentIT.java
@@ -21,7 +21,7 @@ public class CompatibilityHelmChartDeploymentIT {
     @Timeout(value = 20, unit = TimeUnit.MINUTES)
     @ParameterizedTest
     @ValueSource(strings = {"v1.18.20-k3s1", "v1.19.16-k3s1", "v1.20.15-k3s1", "v1.21.10-k3s1", "v1.22.7-k3s1", "v1.23.4-k3s1"})
-    public void withHelmVersionDeployment_mqttMessagePublishedReceived(final @NotNull String version) throws Exception {
+    public void withHelmVersionDeployment_mqttMessagePublishedReceived(final @NotNull String version)  {
         /*try (var container = new OperatorHelmChartContainer(version, "k3s.dockerfile")){
             container.withCopyFileToContainer(MountableFile.forHostPath("charts"), "/charts");
             container.withCopyFileToContainer(MountableFile.forHostPath("build/resources/integrationTest"), "/resources");
