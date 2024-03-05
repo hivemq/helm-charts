9c1aff5ff9b8eb8a616da7c40c9bff41b8777000
David Sondermann <david.sondermann@hivemq.com>
2024-03-05 13:53:43 +0100

    Fix HiveMQExtension to compile extensions with JDK 11

--- tests-hivemq-platform-operator/src/integrationTest/java/com/hivemq/helmcharts/extensions/BaseAppendingPublishInboundInterceptorExtensionMain.java
+++ tests-hivemq-platform-operator/src/integrationTest/java/com/hivemq/helmcharts/extensions/BaseAppendingPublishInboundInterceptorExtensionMain.java
@@ -1,10 +1,6 @@
 package com.hivemq.helmcharts.extensions;
 
-import com.google.common.base.Charsets;
 import com.hivemq.extension.sdk.api.ExtensionMain;
-import com.hivemq.extension.sdk.api.interceptor.publish.PublishInboundInterceptor;
-import com.hivemq.extension.sdk.api.interceptor.publish.parameter.PublishInboundInput;
-import com.hivemq.extension.sdk.api.interceptor.publish.parameter.PublishInboundOutput;
 import com.hivemq.extension.sdk.api.parameter.ExtensionStartInput;
 import com.hivemq.extension.sdk.api.parameter.ExtensionStartOutput;
 import com.hivemq.extension.sdk.api.parameter.ExtensionStopInput;
@@ -14,9 +10,6 @@ import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.nio.ByteBuffer;
-
-@SuppressWarnings("unused")
 abstract class BaseAppendingPublishInboundInterceptorExtensionMain implements ExtensionMain {
 
     private static final @NotNull Logger LOG =
@@ -26,7 +19,7 @@ abstract class BaseAppendingPublishInboundInterceptorExtensionMain implements Ex
     public void extensionStart(
             final @NotNull ExtensionStartInput extensionStartInput,
             final @NotNull ExtensionStartOutput extensionStartOutput) {
-        LOG.info("HiveMQ Appending PublishInboundInterceptor Extension started");
+        LOG.info("{} started", getName());
 
         Services.initializerRegistry()
                 .setClientInitializer((initializerInput, clientContext) -> clientContext.addPublishInboundInterceptor(
@@ -35,34 +28,12 @@ abstract class BaseAppendingPublishInboundInterceptorExtensionMain implements Ex
 
     @Override
     public void extensionStop(
-            @NotNull final ExtensionStopInput extensionStopInput,
-            @NotNull final ExtensionStopOutput extensionStopOutput) {
-        LOG.info("HiveMQ Appending PublishInboundInterceptor Extension stopped");
+            final @NotNull ExtensionStopInput extensionStopInput,
+            final @NotNull ExtensionStopOutput extensionStopOutput) {
+        LOG.info("{} stopped", getName());
     }
 
     abstract @NotNull String getAdditionalPayload();
 
-    private static class AppendingPublishInboundInterceptor implements PublishInboundInterceptor {
-
-        private final @NotNull String logPrefix;
-        private final @NotNull String additionalPayload;
-
-        private AppendingPublishInboundInterceptor(
-                final @NotNull Class<? extends ExtensionMain> extensionClass,
-                final @NotNull String additionalPayload) {
-            this.logPrefix = extensionClass.getSimpleName();
-            this.additionalPayload = additionalPayload;
-        }
-
-        @Override
-        public void onInboundPublish(
-                @NotNull final PublishInboundInput publishInboundInput,
-                @NotNull final PublishInboundOutput publishInboundOutput) {
-            final var publish = publishInboundInput.getPublishPacket();
-            final var payload = Charsets.UTF_8.decode(publish.getPayload().orElse(ByteBuffer.wrap(new byte[0])));
-            final var newPayload = payload + additionalPayload;
-            LOG.info("[{}] Modifying payload: {} -> {}", logPrefix, payload, newPayload);
-            publishInboundOutput.getPublishPacket().setPayload(Charsets.UTF_8.encode(newPayload));
-        }
-    }
+    abstract @NotNull String getName();
 }
