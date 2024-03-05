ecfa4b44653ffc063acbb9b5efaaa3d37805b3c7
David Sondermann <david.sondermann@hivemq.com>
2024-03-05 13:53:43 +0100

    Fix HiveMQExtension to compile extensions with JDK 11

--- tests-hivemq-platform-operator/src/integrationTest/java/com/hivemq/helmcharts/extensions/AppendingBazPublishInboundInterceptorExtensionMain.java
+++ tests-hivemq-platform-operator/src/integrationTest/java/com/hivemq/helmcharts/extensions/AppendingBazPublishInboundInterceptorExtensionMain.java
@@ -3,10 +3,16 @@ package com.hivemq.helmcharts.extensions;
 import org.jetbrains.annotations.NotNull;
 
 @SuppressWarnings("unused")
-public class AppendingBazPublishInboundInterceptorExtensionMain extends BaseAppendingPublishInboundInterceptorExtensionMain {
+public class AppendingBazPublishInboundInterceptorExtensionMain
+        extends BaseAppendingPublishInboundInterceptorExtensionMain {
 
     @Override
     @NotNull String getAdditionalPayload() {
         return "-baz";
+    }
+
+    @Override
+    @NotNull String getName() {
+        return "HiveMQ Baz-Appending PublishInboundInterceptor Extension";
     }
 }
