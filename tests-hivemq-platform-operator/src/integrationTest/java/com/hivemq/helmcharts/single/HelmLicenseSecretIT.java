7e9c45274771964f2917554c1410872a7759fe60
Antonio Fernandez Alhambra <antonio.alhambra@hivemq.com>
2024-03-08 17:41:38 +0100

    Improve license support

--- tests-hivemq-platform-operator/src/integrationTest/java/com/hivemq/helmcharts/single/HelmLicenseSecretIT.java
+++ tests-hivemq-platform-operator/src/integrationTest/java/com/hivemq/helmcharts/single/HelmLicenseSecretIT.java
@@ -2,11 +2,12 @@ package com.hivemq.helmcharts.single;
 
 import com.hivemq.helmcharts.testcontainer.HelmChartContainer;
 import com.hivemq.helmcharts.util.K8sUtil;
-import io.fabric8.kubernetes.api.model.SecretBuilder;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
@@ -25,108 +26,98 @@ import static org.awaitility.Awaitility.await;
 class HelmLicenseSecretIT {
 
     private static final @NotNull String PLATFORM_RELEASE_NAME = "test-hivemq-platform";
+    private static final @NotNull String OPERATOR_RELEASE_NAME = "test-hivemq-platform-operator";
     private static final @NotNull HelmChartContainer HELM_CHART_CONTAINER = new HelmChartContainer();
+
+    private static final @NotNull String NAMESPACE = K8sUtil.getNamespaceName(HelmLicenseSecretIT.class);
 
     @SuppressWarnings("NotNullFieldNotInitialized")
     private static @NotNull KubernetesClient client;
 
     @BeforeAll
     @Timeout(value = 5, unit = TimeUnit.MINUTES)
-    public static void setup() throws Exception {
+    static void start() throws Exception {
         HELM_CHART_CONTAINER.start();
-        HELM_CHART_CONTAINER.installOperatorChart("test-hivemq-platform-operator");
+        HELM_CHART_CONTAINER.installOperatorChart(OPERATOR_RELEASE_NAME);
         client = HELM_CHART_CONTAINER.getKubernetesClient();
+    }
+
+    @BeforeEach
+    @Timeout(value = 5, unit = TimeUnit.MINUTES)
+    void setup() {
+        HELM_CHART_CONTAINER.createNamespace(NAMESPACE);
     }
 
     @AfterAll
     @Timeout(value = 5, unit = TimeUnit.MINUTES)
-    public static void shutdown() {
+    static void shutdown() {
         HELM_CHART_CONTAINER.stop();
+    }
+
+    @AfterEach
+    @Timeout(value = 5, unit = TimeUnit.MINUTES)
+    void tearDown() throws Exception {
+        HELM_CHART_CONTAINER.uninstallRelease(PLATFORM_RELEASE_NAME,
+                "--cascade",
+                "foreground",
+                "--namespace",
+                NAMESPACE);
+        HELM_CHART_CONTAINER.deleteNamespace(NAMESPACE);
     }
 
     @Test
     @Timeout(value = 5, unit = TimeUnit.MINUTES)
     void withLicenseFileContent_statefulSetWithLicenseSecretMounted() throws Exception {
         final var customResourceName = "test-hivemq-platform";
-        final var namespace = "license-file-content";
-        HELM_CHART_CONTAINER.createNamespace(namespace);
-
         HELM_CHART_CONTAINER.installPlatformChart(PLATFORM_RELEASE_NAME,
                 "--namespace",
-                namespace,
+                NAMESPACE,
+                "--set",
+                "nodes.replicaCount=1",
+                "--set",
+                "license.create=true",
                 "--set-file",
-                "license.fileContent=/files/mock-license.lic");
-
-        await().atMost(Duration.ofMinutes(2)).pollInterval(Duration.ofSeconds(5)).untilAsserted(() -> {
-            final var statefulSet =
-                    client.apps().statefulSets().inNamespace(namespace).withName(customResourceName).get();
-            assertThat(statefulSet).isNotNull();
-            final var volumes = statefulSet.getSpec().getTemplate().getSpec().getVolumes();
-            assertThat(volumes).isNotNull();
-            final var licenseVolume = statefulSet.getSpec()
-                    .getTemplate()
-                    .getSpec()
-                    .getVolumes()
-                    .stream()
-                    .filter(v -> v.getName().equals("licenses"))
-                    .findFirst();
-            assertThat(licenseVolume).isNotNull();
-        });
-        HELM_CHART_CONTAINER.uninstallRelease(PLATFORM_RELEASE_NAME,
-                "--cascade",
-                "foreground",
-                "--namespace",
-                namespace);
-        HELM_CHART_CONTAINER.deleteNamespace(namespace);
+                "license.overrideLicense=/files/mock-license.lic");
+        K8sUtil.waitForHiveMQPlatformStateRunning(client, NAMESPACE, customResourceName);
+        assertLicense(customResourceName, "hivemq-license-test-hivemq-platform");
     }
 
     @Test
     @Timeout(value = 5, unit = TimeUnit.MINUTES)
     void withExistingSecret_statefulSetWithLicenseSecretMounted() throws Exception {
         final var customResourceName = "test-hivemq-platform";
-        final var namespace = "existing-license-secret";
-        HELM_CHART_CONTAINER.createNamespace(namespace);
-
-        final var licenseSecret = new SecretBuilder().withNewMetadata()
-                .withName("test-license")
-                .endMetadata()
-                .withData(Map.of("license.lic",
-                        Base64.getEncoder().encodeToString("license data".getBytes(StandardCharsets.UTF_8))))
-                .build();
-
-        client.secrets().inNamespace(namespace).resource(licenseSecret).create();
-
+        K8sUtil.createSecret(client,
+                NAMESPACE,
+                "test-license",
+                Map.of("license.lic",
+                        Base64.getEncoder().encodeToString("license data".getBytes(StandardCharsets.UTF_8))));
         HELM_CHART_CONTAINER.installPlatformChart(PLATFORM_RELEASE_NAME,
                 "--namespace",
-                namespace,
+                NAMESPACE,
+                "--set",
+                "nodes.replicaCount=1",
                 "--set",
                 "license.name=test-license");
+        K8sUtil.waitForHiveMQPlatformStateRunning(client, NAMESPACE, customResourceName);
+        assertLicense(customResourceName, "test-license");
+    }
 
+    @SuppressWarnings("SameParameterValue")
+    private void assertLicense(
+            final @NotNull String customResourceName, final @NotNull String licenseSecretName) {
         await().atMost(Duration.ofMinutes(2)).pollInterval(Duration.ofSeconds(5)).untilAsserted(() -> {
             final var statefulSet =
-                    client.apps().statefulSets().inNamespace(namespace).withName("test-hivemq-platform").get();
+                    client.apps().statefulSets().inNamespace(NAMESPACE).withName(customResourceName).get();
             assertThat(statefulSet).isNotNull();
+            assertThat(K8sUtil.getHiveMQContainer(statefulSet.getSpec()).getVolumeMounts().stream()) //
+                    .anyMatch(volumeMount -> volumeMount.getName().equals("licenses") &&
+                            volumeMount.getMountPath().equals("/opt/hivemq/license"));
+
             final var volumes = statefulSet.getSpec().getTemplate().getSpec().getVolumes();
             assertThat(volumes).isNotNull();
-            final var licenseVolume = statefulSet.getSpec()
-                    .getTemplate()
-                    .getSpec()
-                    .getVolumes()
-                    .stream()
-                    .filter(v -> v.getName().equals("licenses"))
-                    .findFirst();
-            assertThat(licenseVolume).isPresent()
-                    .get()
-                    .satisfies(licVolume -> assertThat(licVolume.getSecret()
-                            .getSecretName()).isEqualTo("test-license"));
+            assertThat(volumes.stream()) //
+                    .anyMatch(volume -> volume.getName().equals("licenses") &&
+                            volume.getSecret().getSecretName().equals(licenseSecretName));
         });
-        K8sUtil.waitForHiveMQPlatformStateRunning(client, namespace, customResourceName);
-
-        HELM_CHART_CONTAINER.uninstallRelease(PLATFORM_RELEASE_NAME,
-                "--cascade",
-                "foreground",
-                "--namespace",
-                namespace);
-        HELM_CHART_CONTAINER.deleteNamespace(namespace);
     }
 }
