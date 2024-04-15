095a0998dc95f7d197b0b7ff50477205bdb6f7ac
Silvio Giebl <silvio.giebl@hivemq.com>
2024-04-15 17:04:13 +0200

    build > use = instead of .set()

--- tests-hivemq-operator/build.gradle.kts
+++ tests-hivemq-operator/build.gradle.kts
@@ -8,7 +8,7 @@ val k8sVersion = "k8s-${project.properties["hivemq.version"]}"
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
+        languageVersion = JavaLanguageVersion.of(21)
     }
 }
 
