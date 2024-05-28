37ce36579f2e6982754d7a74a8314dd4e246c5be
Antonio Fernandez Alhambra <antonio.alhambra@hivemq.com>
2024-05-28 11:26:37 +0200

    Added integration test for `additionalContainers` and `additionalInitContainers`

--- tests-hivemq-platform-operator/src/integrationTest/java/com/hivemq/helmcharts/single/HelmInitContainerIT.java
+++ tests-hivemq-platform-operator/src/integrationTest/java/com/hivemq/helmcharts/single/HelmInitContainerIT.java
@@ -1,6 +1,7 @@
 package com.hivemq.helmcharts.single;
 
 import com.hivemq.helmcharts.AbstractHelmChartIT;
+import com.hivemq.helmcharts.util.K8sUtil;
 import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.api.model.Volume;
 import io.fabric8.kubernetes.api.model.VolumeMount;
@@ -12,18 +13,20 @@ import org.junit.jupiter.api.Timeout;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 
 @Tag("CustomConfig")
-@Tag("Volumes")
+@Tag("Containers")
+@SuppressWarnings("DuplicatedCode")
 class HelmInitContainerIT extends AbstractHelmChartIT {
 
     @Test
     @Timeout(value = 5, unit = TimeUnit.MINUTES)
-    void withAdditionalInitContainer_hivemqRunningWithVolumeMounts() throws Exception {
+    void withOverrideInitContainer_hivemqRunningWithVolumeMounts() throws Exception {
         final String additionalVolumeFile = "/files/init-container-additional-volumes-test-values.yaml";
         final String additionalInitContainerFile = "/files/init-containers-spec.yaml";
 
@@ -50,6 +53,36 @@ class HelmInitContainerIT extends AbstractHelmChartIT {
                     .isNotNull() //
                     .satisfies(this::assertThatFileContains));
         });
+    }
+
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.MINUTES)
+    void whenAdditionalInitContainer_withConsulTemplate_thenLicenseUpdated() throws Exception {
+        K8sUtil.createConfigMap(client,
+                platformNamespace,
+                "consul-template-config-map.yml");
+        installPlatformChartAndWaitToBeRunning("/files/additional-init-containers-test-values.yaml");
+
+        await().atMost(Duration.ofMinutes(2)).pollInterval(Duration.ofSeconds(5)).untilAsserted(() -> {
+            final var statefulSet =
+                    client.apps().statefulSets().inNamespace(platformNamespace).withName(PLATFORM_RELEASE_NAME).get();
+            assertThat(statefulSet).isNotNull();
+            final var hivemqContainer = K8sUtil.getHiveMQContainer(statefulSet.getSpec());
+            assertThat(hivemqContainer.getVolumeMounts().stream()) //
+                    .anyMatch(volumeMount -> volumeMount.getName().equals("license-volume") &&
+                            volumeMount.getMountPath().equals("/opt/hivemq/license"));
+            final var additionalInitContainer = K8sUtil.getInitContainer(statefulSet.getSpec(), "consul-template-init-container");
+            assertThat(additionalInitContainer.getVolumeMounts().stream()) //
+                    .anyMatch(volumeMount -> volumeMount.getName().equals("license-volume") &&
+                            volumeMount.getMountPath().equals("/opt/hivemq/license"));
+
+            final var volumes = statefulSet.getSpec().getTemplate().getSpec().getVolumes();
+            assertThat(volumes).isNotNull();
+            assertThat(volumes.stream()) //
+                    .anyMatch(volume -> volume.getName().equals("license-volume") &&
+                            volume.getEmptyDir() != null);
+        });
+        waitForPlatformLog(".*License file license.lic is corrupt.");
     }
 
     private void assertThatFileContains(final @NotNull Pod pod) {
