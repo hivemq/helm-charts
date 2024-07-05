354422fa87ddbd3270094453212a829fa331bf60
David Sondermann <david.sondermann@hivemq.com>
2024-07-05 18:04:11 +0200

    Pull out manifests-common.sh script to ease usage in custom scripts

--- manifests/hivemq-platform/manifests.sh
+++ manifests/hivemq-platform/manifests.sh
@@ -1,26 +1,11 @@
 #!/bin/sh
 
-echo "========================================="
-echo "Generating HiveMQ Platform Manifest files"
+HELM_CHARTS_BASE_FOLDER="$(cd -- "$(dirname -- "$0")/../.." && pwd)"
+. "${HELM_CHARTS_BASE_FOLDER}/manifests/manifests-common.sh"
 
-# change current directory to project root
-cd "$(dirname "$0")"/../.. || exit 1
+export LABEL="HiveMQ Platform"
+export CHART_NAME="hivemq-platform"
+export RELEASE_NAME="${1:-my-platform}"
+export HELM_CHARTS_BASE_FOLDER
 
-echo "Create manifests directory"
-mkdir -p manifests && cd manifests || exit 1
-
-echo "Create HiveMQ Platform Templates"
-helm template my-platform ../charts/hivemq-platform --skip-tests --output-dir . > /dev/null
-
-echo "Flatten directory structure"
-find hivemq-platform/templates -type f -exec mv {} hivemq-platform/ \; > /dev/null
-if [ -d hivemq-platform/templates ]; then
-  rm -r hivemq-platform/templates
-fi
-
-echo "Remove Helm annotations"
-# The BSD implementation of sed does NOT support case-insensitive matching
-sed -i.bak '/helm/d' hivemq-platform/*.yml
-sed -i.bak '/Helm/d' hivemq-platform/*.yml
-sed -i.bak '/namespace:/d' hivemq-platform/*.yml
-find . -type f -name "*.bak" -delete
+update_manifests
--- manifests/hivemq-swarm/manifests.sh
+++ manifests/hivemq-swarm/manifests.sh
@@ -1,35 +1,12 @@
 #!/bin/sh
 
-PROMETHEUS_HELM_CHARTS_URL=https://prometheus-community.github.io/helm-charts
+HELM_CHARTS_BASE_FOLDER="$(cd -- "$(dirname -- "$0")/../.." && pwd)"
+. "${HELM_CHARTS_BASE_FOLDER}/manifests/manifests-common.sh"
 
-echo "======================================"
-echo "Generating HiveMQ Swarm Manifest files"
+export LABEL="HiveMQ Swarm"
+export CHART_NAME="hivemq-swarm"
+export RELEASE_NAME="${1:-my-swarm}"
+export HELM_CHARTS_BASE_FOLDER
+export PROMETHEUS_HELM_CHARTS_URL=https://prometheus-community.github.io/helm-charts
 
-# change current directory to project root
-cd "$(dirname "$0")"/../.. || exit 1
-
-echo "Create manifests directory"
-mkdir -p manifests && cd manifests || exit 1
-
-echo "Update HiveMQ Swarm dependencies"
-prometheus_repo=$(helm repo list -o json | jq -r '.[] | select(.url == "'${PROMETHEUS_HELM_CHARTS_URL}'") | .name')
-if [ -z "$prometheus_repo" ]; then
-  echo "Adding Prometheus Helm Chart dependency"
-  helm repo add prometheus ${PROMETHEUS_HELM_CHARTS_URL}
-fi
-helm dependency build ../charts/hivemq-swarm
-
-echo "Create HiveMQ Swarm Templates"
-helm template my-swarm ../charts/hivemq-swarm --skip-tests --output-dir . > /dev/null
-
-echo "Flatten directory structure"
-find hivemq-swarm/templates -type f -exec mv {} hivemq-swarm/ \; > /dev/null
-if [ -d hivemq-swarm/templates ]; then
-  rm -r hivemq-swarm/templates
-fi
-
-echo "Remove Helm annotations"
-# The BSD implementation of sed does NOT support case-insensitive matching
-sed -i.bak '/helm/d' hivemq-swarm/*.yaml
-sed -i.bak '/Helm/d' hivemq-swarm/*.yaml
-find . -type f -name "*.bak" -delete
+update_manifests
