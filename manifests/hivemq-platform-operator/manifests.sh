0aa5be31d3b2846646cfd0d47102ffa250d0ebd0
David Sondermann <david.sondermann@hivemq.com>
2024-07-05 18:04:11 +0200

    Pull out manifests-common.sh script to ease usage in custom scripts

--- manifests/hivemq-platform-operator/manifests.sh
+++ manifests/hivemq-platform-operator/manifests.sh
@@ -1,25 +1,11 @@
 #!/bin/sh
 
-echo "=================================================="
-echo "Generating HiveMQ Platform Operator Manifest files"
+HELM_CHARTS_BASE_FOLDER="$(cd -- "$(dirname -- "$0")/../.." && pwd)"
+. "${HELM_CHARTS_BASE_FOLDER}/manifests/manifests-common.sh"
 
-# change current directory to project root
-cd "$(dirname "$0")"/../.. || exit 1
+export LABEL="HiveMQ Platform Operator"
+export CHART_NAME="hivemq-platform-operator"
+export RELEASE_NAME="${1:-my-operator}"
+export HELM_CHARTS_BASE_FOLDER
 
-echo "Create manifests directory"
-mkdir -p manifests && cd manifests || exit 1
-
-echo "Create Platform Operator Templates"
-helm template my-operator ../charts/hivemq-platform-operator --skip-tests --output-dir . > /dev/null
-
-echo "Flatten directory structure"
-find hivemq-platform-operator/templates -type f -exec mv {} hivemq-platform-operator/ \; > /dev/null
-if [ -d hivemq-platform-operator/templates ]; then
-  rm -r hivemq-platform-operator/templates
-fi
-
-echo "Remove Helm annotations"
-# The BSD implementation of sed does NOT support case-insensitive matching
-sed -i.bak '/helm/d' hivemq-platform-operator/*.yml
-sed -i.bak '/Helm/d' hivemq-platform-operator/*.yml
-find . -type f -name "*.bak" -delete
+update_manifests
