8b149cf1b6ba6167d6604f1454e60ff9774dbaa2
Antonio Fernandez Alhambra <antonio.alhambra@hivemq.com>
2025-01-13 12:49:32 +0100

    Update K3s rancher docker image to v1.32

--- tests-hivemq-operator/src/integrationTest/java/com/hivemq/helmcharts/testcontainer/DockerImageNames.java
+++ tests-hivemq-operator/src/integrationTest/java/com/hivemq/helmcharts/testcontainer/DockerImageNames.java
@@ -8,7 +8,7 @@ public class DockerImageNames {
     }
 
     public enum K3s {
-        // https://hub.docker.com/r/rancher/k3s/tags?page=1&name=v1.31
+        // https://hub.docker.com/r/rancher/k3s/tags?page=1&name=v1.32
         V1_20("v1.20.15-k3s1"),
         V1_21("v1.21.14-k3s1"),
         V1_22("v1.22.17-k3s1"),
@@ -17,14 +17,15 @@ public class DockerImageNames {
         V1_25("v1.25.16-k3s4"),
         V1_26("v1.26.15-k3s1"),
         V1_27("v1.27.16-k3s1"),
-        V1_28("v1.28.13-k3s1"),
-        V1_29("v1.29.8-k3s1"),
-        V1_30("v1.30.4-k3s1"),
-        V1_31("v1.31.0-k3s1"),
+        V1_28("v1.28.15-k3s1"),
+        V1_29("v1.29.12-k3s1"),
+        V1_30("v1.30.8-k3s1"),
+        V1_31("v1.31.4-k3s1"),
+        V1_32("v1.32.0-k3s1"),
 
         MINIMUM(V1_20.version),
         DEFAULT(V1_27.version),
-        LATEST(V1_31.version);
+        LATEST(V1_32.version);
 
         private final @NotNull String version;
 
