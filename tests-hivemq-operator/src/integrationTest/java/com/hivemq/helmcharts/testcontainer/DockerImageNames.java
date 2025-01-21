9ec4a03bc2fc0c3700133a89e333d552b237e01e
David Sondermann <david.sondermann@hivemq.com>
2025-01-21 14:04:34 +0000

    Pull out Docker images into custom version catalog

--- tests-hivemq-operator/src/integrationTest/java/com/hivemq/helmcharts/testcontainer/DockerImageNames.java
+++ tests-hivemq-operator/src/integrationTest/java/com/hivemq/helmcharts/testcontainer/DockerImageNames.java
@@ -1,40 +1,15 @@
 package com.hivemq.helmcharts.testcontainer;
 
 import org.jetbrains.annotations.NotNull;
+import org.testcontainers.utility.DockerImageName;
 
 public class DockerImageNames {
 
     private DockerImageNames() {
     }
 
-    public enum K3s {
-        // https://hub.docker.com/r/rancher/k3s/tags?page=1&name=v1.32
-        V1_20("v1.20.15-k3s1"),
-        V1_21("v1.21.14-k3s1"),
-        V1_22("v1.22.17-k3s1"),
-        V1_23("v1.23.17-k3s1"),
-        V1_24("v1.24.17-k3s1"),
-        V1_25("v1.25.16-k3s4"),
-        V1_26("v1.26.15-k3s1"),
-        V1_27("v1.27.16-k3s1"),
-        V1_28("v1.28.15-k3s1"),
-        V1_29("v1.29.12-k3s1"),
-        V1_30("v1.30.8-k3s1"),
-        V1_31("v1.31.4-k3s1"),
-        V1_32("v1.32.0-k3s1"),
-
-        MINIMUM(V1_20.version),
-        DEFAULT(V1_27.version),
-        LATEST(V1_32.version);
-
-        private final @NotNull String version;
-
-        K3s(final @NotNull String version) {
-            this.version = version;
-        }
-
-        public @NotNull String getVersion() {
-            return version;
-        }
-    }
+    // test the latest supported version for the legacy Helm chart
+    // https://hub.docker.com/r/rancher/k3s/tags?page=1&name=v1.27
+    public static final @NotNull DockerImageName K3S_DOCKER_IMAGE =
+            DockerImageName.parse("rancher/k3s").withTag("v1.27.16-k3s1").asCompatibleSubstituteFor("rancher/k3s");
 }
