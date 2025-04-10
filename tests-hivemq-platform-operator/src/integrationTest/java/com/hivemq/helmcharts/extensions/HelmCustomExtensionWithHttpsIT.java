1c228beb4f65e590cb4dc3cb0f8a38deb28e7bf5
David Sondermann <david.sondermann@hivemq.com>
2025-04-10 13:00:24 +0200

    Split HelmCustomExtensionIT

--- tests-hivemq-platform-operator/src/integrationTest/java/com/hivemq/helmcharts/extensions/HelmCustomExtensionWithHttpsIT.java
+++ tests-hivemq-platform-operator/src/integrationTest/java/com/hivemq/helmcharts/extensions/HelmCustomExtensionWithHttpsIT.java
@@ -3,10 +3,8 @@ package com.hivemq.helmcharts.extensions;
 import com.hivemq.helmcharts.AbstractHelmChartIT;
 import com.hivemq.helmcharts.extensions.custom.CustomTestExtensionMain;
 import com.hivemq.helmcharts.util.HiveMQExtension;
-import com.hivemq.helmcharts.util.K8sUtil;
 import com.hivemq.helmcharts.util.NginxUtil;
 import org.jetbrains.annotations.NotNull;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.api.io.TempDir;
@@ -18,24 +16,24 @@ import java.util.concurrent.TimeUnit;
 import static org.awaitility.Awaitility.await;
 import static org.awaitility.Durations.ONE_MINUTE;
 
-class HelmCustomExtensionIT extends AbstractHelmChartIT {
+class HelmCustomExtensionWithHttpsIT extends AbstractHelmChartIT {
 
     @TempDir
     private @NotNull Path tmp;
 
     @Test
     @Timeout(value = 5, unit = TimeUnit.MINUTES)
-    void platformChart_whenCustomExtensionIsConfigured_withHttp_thenStartsUpSuccessfully() throws Exception {
+    void platformChart_whenCustomExtensionIsConfigured_withHttps_thenStartsUpSuccessfully() throws Exception {
         final var customExtensionZip = HiveMQExtension.createHiveMQExtensionZip(tmp,
                 "hivemq-custom-test-extension",
                 "HiveMQ Custom Test Extension",
-                "1.0.0",
+                "3.0.0",
                 CustomTestExtensionMain.class);
-        NginxUtil.deployNginx(client, platformNamespace, helmChartContainer, List.of(customExtensionZip), false, false);
+        NginxUtil.deployNginx(client, platformNamespace, helmChartContainer, List.of(customExtensionZip), true, false);
         final var extensionStartedFuture =
-                waitForPlatformLog(".*Extension \"HiveMQ Custom Test Extension\" version 1.0.0 started successfully.");
+                waitForPlatformLog(".*Extension \"HiveMQ Custom Test Extension\" version 3.0.0 started successfully.");
 
-        installPlatformChartAndWaitToBeRunning("/files/custom-extension-values.yaml");
+        installPlatformChartAndWaitToBeRunning("/files/custom-extension-values-with-https.yaml");
         await().atMost(ONE_MINUTE).until(extensionStartedFuture::isDone);
     }
 }
