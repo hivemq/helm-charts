204714b286406cd38dd2b77d3133e70f833334cd
David Sondermann <david.sondermann@hivemq.com>
2025-12-05 14:29:50 +0100

    Split HelmExtensionLicensesIT

--- tests-hivemq-platform-operator/src/integrationTest/java/com/hivemq/helmcharts/license/HelmExtensionExistingSecretLicensesIT.java
+++ tests-hivemq-platform-operator/src/integrationTest/java/com/hivemq/helmcharts/license/HelmExtensionExistingSecretLicensesIT.java
@@ -4,15 +4,18 @@ import com.hivemq.helmcharts.util.K8sUtil;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import static org.awaitility.Awaitility.await;
 
-class HelmExtensionLicensesIT extends AbstractHelmLicensesIT {
+class HelmExtensionExistingSecretLicensesIT extends AbstractHelmLicensesIT {
 
     @Test
     @Timeout(value = 5, unit = TimeUnit.MINUTES)
-    void withExtensionLicenseFileContent_statefulSetWithLicenseSecretMounted() throws Exception {
+    void withExistingExtensionLicenseSecret_statefulSetWithLicenseSecretMounted() throws Exception {
         K8sUtil.createConfigMap(client, platformNamespace, "distributed-tracing-config-map.yml");
 
         final var extensionStartedFuture = waitForPlatformLog(
@@ -20,13 +23,17 @@ class HelmExtensionLicensesIT extends AbstractHelmLicensesIT {
         final var extensionLicenseFuture =
                 logWaiter.waitFor(PLATFORM_LOG_WAITER_PREFIX, ".*License file tracing.elic is corrupt.");
 
+        K8sUtil.createSecret(client,
+                platformNamespace,
+                "tracing-extension-secret-license",
+                Map.of("tracing.elic",
+                        Base64.getEncoder()
+                                .encodeToString("tracing extension license data".getBytes(StandardCharsets.UTF_8))));
         installPlatformChartAndWaitToBeRunning("-f",
                 "/files/distributed-tracing-extension-values.yaml",
                 "--set",
-                "license.create=true",
-                "--set-file",
-                "license.extensions.tracing.overrideLicense=/files/mock-extension-license.elic");
-        assertLicense("hivemq-license-test-hivemq-platform");
+                "license.name=tracing-extension-secret-license");
+        assertLicense("tracing-extension-secret-license");
         await().until(extensionStartedFuture::isDone);
         await().until(extensionLicenseFuture::isDone);
     }
