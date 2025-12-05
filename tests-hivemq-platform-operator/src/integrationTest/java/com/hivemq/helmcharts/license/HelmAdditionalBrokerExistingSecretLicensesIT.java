2723406ffea9cbf915121cb45bcd3366f02bc05a
David Sondermann <david.sondermann@hivemq.com>
2025-12-05 14:26:58 +0100

    Split HelmAdditionalBrokerLicensesIT

---
+++ tests-hivemq-platform-operator/src/integrationTest/java/com/hivemq/helmcharts/license/HelmAdditionalBrokerExistingSecretLicensesIT.java
@@ -0,0 +1,34 @@
+package com.hivemq.helmcharts.license;
+
+import com.hivemq.helmcharts.util.K8sUtil;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static org.awaitility.Awaitility.await;
+
+class HelmAdditionalBrokerExistingSecretLicensesIT extends AbstractHelmLicensesIT {
+
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.MINUTES)
+    void withExistingAdditionalBrokerLicenseFileContent_statefulSetWithLicenseSecretMounted() throws Exception {
+        final var brokerLicenseFuture =
+                logWaiter.waitFor(PLATFORM_LOG_WAITER_PREFIX, ".*License file license.lic is corrupt.");
+        K8sUtil.createSecret(client,
+                platformNamespace,
+                "test-additional-broker-license",
+                Map.of("license.lic",
+                        Base64.getEncoder()
+                                .encodeToString("additional broker license data".getBytes(StandardCharsets.UTF_8))));
+        installPlatformChartAndWaitToBeRunning("--set",
+                "nodes.replicaCount=1",
+                "--set",
+                "license.name=test-additional-broker-license");
+        assertLicense("test-additional-broker-license");
+        await().until(brokerLicenseFuture::isDone);
+    }
+}
