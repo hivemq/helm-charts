54d6c2cd21ebf857e815d5c3a756ddfc6da70562
David Sondermann <david.sondermann@hivemq.com>
2025-12-05 14:40:02 +0100

    Split HelmControlCenterIT

---
+++ tests-hivemq-platform-operator/src/integrationTest/java/com/hivemq/helmcharts/services/HelmControlCenterCustomCredentialsIT.java
@@ -0,0 +1,24 @@
+package com.hivemq.helmcharts.services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.hivemq.helmcharts.util.ControlCenterUtil.assertLogin;
+
+class HelmControlCenterCustomCredentialsIT extends AbstractHelmControlCenterIT {
+
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.MINUTES)
+    void platformChart_withCustomControlCenterUsernameAndPassword_thenAbleToLogin() throws Exception {
+        installPlatformChartAndWaitToBeRunning("/files/control-center-basic-credentials-values.yaml");
+        assertLogin(client,
+                platformNamespace,
+                WEB_DRIVER_CONTAINER,
+                CC_SERVICE_NAME_8081,
+                CC_SERVICE_PORT_8081,
+                "test-username",
+                "test-password");
+    }
+}
