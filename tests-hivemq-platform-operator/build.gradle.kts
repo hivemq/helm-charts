c01375f2a3226bc4c92e9ec0e3e03f06549ed27f
David Sondermann <david.sondermann@hivemq.com>
2026-03-02 11:10:40 +0100

    Refactor OCI image dependencies for Helm tasks

--- tests-hivemq-platform-operator/build.gradle.kts
+++ tests-hivemq-platform-operator/build.gradle.kts
@@ -129,7 +129,9 @@ tasks.register("integrationTestPrepare") {
 /* ******************** OCI images ******************** */
 
 val helmOciLayerLinuxAmd64 by tasks.registering(oci.dockerLayerTaskClass) {
-    from = "library/ubuntu@sha256:cd1dba651b3080c3686ecf4e3c4220f026b521fb76978881737d24f200828b2b" // noble
+    dependencies(oci.parentImageDependencies.create("noble") {
+        runtime("library:ubuntu:sha256!cd1dba651b3080c3686ecf4e3c4220f026b521fb76978881737d24f200828b2b") // noble
+    })
     platform = oci.platform("linux", "amd64")
     command =
         "apt-get update && apt-get install --no-install-recommends curl apt-transport-https ca-certificates -yq && " +
@@ -140,7 +142,9 @@ val helmOciLayerLinuxAmd64 by tasks.registering(oci.dockerLayerTaskClass) {
 }
 
 val helmOciLayerLinuxArm64 by tasks.registering(oci.dockerLayerTaskClass) {
-    from = "library/ubuntu@sha256:cd1dba651b3080c3686ecf4e3c4220f026b521fb76978881737d24f200828b2b" // noble
+    dependencies(oci.parentImageDependencies.create("noble") {
+        runtime("library:ubuntu:sha256!cd1dba651b3080c3686ecf4e3c4220f026b521fb76978881737d24f200828b2b") // noble
+    })
     platform = oci.platform("linux", "arm64", "v8")
     command =
         "apt-get update && apt-get install --no-install-recommends curl apt-transport-https ca-certificates -yq && " +
